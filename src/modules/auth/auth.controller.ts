a628244dcc76f87b1462dfae8dc0ea07f30de040
kahonoo <kahonowidodo@gmail.com>
2025-07-30 11:26:51 +0700

    feat: add forgot password (#2)

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -16,4 +16,8 @@ export class AuthController {
     const result = await this.authService.login(req.body);
     res.status(200).send(result);
   };
+  forgotPassword = async (req: Request, res: Response) => {
+    const result = await this.authService.forgotPassword(req.body);
+    res.status(200).send(result);
+  };
 }
